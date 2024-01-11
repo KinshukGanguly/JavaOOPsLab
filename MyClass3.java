58ad35a4c50b8193aa38d580f9aaba8626297507
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2024-01-11 12:29:15 +0530

    Update MyClass3.java

--- MyClass3.java
+++ MyClass3.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-class MyClass4
+class MyClass3
 {
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter number 1-");
-int a=sc.nextInt();
-System.out.println("Enter number 2-");
-int b=sc.nextInt();
-System.out.println("Result: "+a+"+"+b+"="+(a+b));
+System.out.println("Enter name");
+String name=sc.nextLine();
+System.out.println("Entered name-"+name);
 }
 }
