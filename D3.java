5121c32d374b37236e6b9f563d294bdc4dede9f4
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2024-01-18 12:37:29 +0530

    Add files via upload

---
+++ D3.java
@@ -0,0 +1,25 @@
+class D3
+{
+
+ int x=0;
+ int y=0;
+ 
+ D3(int x,int y)
+ {
+  x=x;
+  y=y;
+  //System.out.println("x="+x+" y="+y);  
+ }
+
+
+public static void main(String args[])
+
+ {
+    D3 obj=new D3(56,58);
+   System.out.println("x="+obj.x+" y="+obj.y);  
+ 
+ }
+ 
+
+
+}
