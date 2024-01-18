0f8f05caec826b831c7f053b9001d3cf0d99b246
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2024-01-18 11:45:27 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,16 @@
+class C
+{public static void main(String args[])
+
+ {
+    for (int i=0;i<args.length;i++)
+    {
+     System.out.println("Month"+(i+1)+" "+args[i]);
+    
+    
+    
+    }   
+ 
+ 
+ }
+
+}
