a4d8ad1baedace5aa3693743fc0ef22ffa34fe0c
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2024-01-25 12:26:40 +0530

    Add files via upload

---
+++ MyClass12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MyClass12
+{
+ 
+ public static void main(String args[])
+ {  //Scanner sc=new Scanner(System.in);
+   int a[]={100,90,60,20};
+   
+   /*int[] myArray=new int[3];
+   [] myArray={1,2,3};
+      
+           
+   int[] myArray=new int[]{1,2,3,4};
+   */
+   
+   for(int element:a)
+   {
+    System.out.println(element);
+   }
+   
+  } 
+ 
+ 
+ }
+
+
+
