667ea51ade6392b683afe3d3f2d283feede4d546
KinshukGanguly <121783408+KinshukGanguly@users.noreply.github.com>
2024-01-25 12:07:19 +0530

    Add files via upload

---
+++ MyClass9.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class MyClass9
+{
+ 
+ public static void main(String args[])
+ {  //Scanner sc=new Scanner(System.in);
+   int myArray[]={100,90,60,20};
+   
+   /*int[] myArray=new int[3];
+   [] myArray={1,2,3};
+   
+   
+           
+   int[] myArray=new int[]{1,2,3,4};
+   */
+   
+   
+   int i=0;
+   
+   while(i<=3)
+   {
+    System.out.println(myArray[i]);
+    i++;
+   }
+   
+  } 
+ 
+ 
+ }
+
+
+
